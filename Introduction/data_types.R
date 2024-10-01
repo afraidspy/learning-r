7ff770b095517e13847fb68193e0d4fad9bca81b
Jessica Santizo Galicia <js@ciencias.unam.mx>
2024-10-01 11:02:05 +0100

    Adding different types in R

---
+++ Introduction/data_types.R
@@ -0,0 +1,23 @@
+
+# Numeric (integer and double)
+num1 <- 10          # integer
+num2 <- 3.14        # double
+
+# Character (String)
+char <- "Hello, R!"
+
+# Logical (Boolean)
+log_val <- TRUE
+
+# Complex
+complex_val <- 2+3i
+
+# Factor (Categorical)
+factor_val <- factor(c("apple", "banana", "apple", "orange"))
+
+# Data Frame
+data_frame_val <- data.frame(Name = c("Alice", "Bob"), Age = c(25, 30))
+
+# List
+list_val <- list(num1, char, log_val, data_frame_val)
+
